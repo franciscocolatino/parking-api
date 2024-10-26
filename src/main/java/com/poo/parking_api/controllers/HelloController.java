ab22e23a0e6f2bf1e8784903f762a94126eaf7c7
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-10-26 19:15:54 -0300

    enabled EnabledMethodSecurity and add basic example in hello page

--- src/main/java/com/poo/parking_api/controllers/HelloController.java
+++ src/main/java/com/poo/parking_api/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package com.poo.parking_api.controllers;
 
+import com.poo.parking_api.domain.user.UserRole;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
     @GetMapping("/hello")
+    //@PreAuthorize("hasRole(UserRole.ADMIN)") COLOCA ISSO NA ROTA QUE QUISER BLOQUEAR
     public String hello(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         model.addAttribute("message", "Hello, Thymeleaf!");
         model.addAttribute("name", userDetails.getUsername());
         model.addAttribute("roles", userDetails.getAuthorities());
+
         return "hello";
     }
 }
