e9c16e9fd6f9ed77bb5f5c47c7390c38fdc1e173
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-11-04 22:29:49 -0300

    feat: add index and show to crud of users

--- src/main/java/com/poo/parking_api/controllers/UserController.java
+++ src/main/java/com/poo/parking_api/controllers/UserController.java
@@ -3,24 +3,55 @@ package com.poo.parking_api.controllers;
 import com.poo.parking_api.domain.user.RequestUser;
 import com.poo.parking_api.domain.user.User;
 import com.poo.parking_api.domain.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-@RestController
-@RequestMapping("/users")
+@Controller
 public class UserController {
 
     @Autowired
     private UserRepository repository;
-    @GetMapping
-    public ResponseEntity index() {
-        var users = repository.findAll();
-        return ResponseEntity.ok(users);
+
+    @GetMapping("/users")
+    public String index(Model model) {
+        List<User> users = repository.findAll();
+        model.addAttribute("users", users);
+        return "users/index";
+    }
+
+    @GetMapping("users/{id}")
+    public String show(@PathVariable String id, Model model) {
+        Optional<User> user = repository.findById(id);
+        if (user.isPresent()) {
+            model.addAttribute("user", user.get());
+        } else {
+            return "redirect:/users?error=userNotFound";
+        }
+        return "users/show";
+    }
+
+    @DeleteMapping("users/{id}")
+    @Transactional
+    public ResponseEntity<User> delete(@PathVariable String id) {
+        Optional<User> optionalUser = repository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            repository.delete(user);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
+
     }
 
     @PostMapping
@@ -42,5 +73,6 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
     }
+
 
 }
