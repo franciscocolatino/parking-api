3176e43d1c606c9be11c2fa8de127decada05712
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-11-17 23:25:07 -0300

    adjust profile and sidebar

--- src/main/java/com/poo/parking_api/controllers/ProfileController.java
+++ src/main/java/com/poo/parking_api/controllers/ProfileController.java
@@ -1,6 +1,8 @@
 package com.poo.parking_api.controllers;
 
 
+import com.poo.parking_api.domain.user.User;
+import com.poo.parking_api.domain.user.UserRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -9,10 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
+    private final UserRepository userRepository;
+
+    public ProfileController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/profile")
     public String profile(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-        model.addAttribute("name", userDetails.getUsername());
-        model.addAttribute("roles", userDetails.getAuthorities());
+        User user = userRepository.findByName(userDetails.getUsername()).get();
+        model.addAttribute("user", user);
+        model.addAttribute("role", userDetails.getAuthorities().stream().findFirst().get().getAuthority());
 
         return "profile/index";
     }
